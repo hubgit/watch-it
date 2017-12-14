cc5f2e1e9c5bdc532ade97290a901a59bcafe1f2
Alf Eaton <eaton.alf@gmail.com>
2017-12-14 22:40:30 +0000

    Limit to 3 results

--- search.js
+++ search.js
@@ -3,6 +3,7 @@ const search = q => {
   params.append('key', 'AIzaSyBuCcm-Q9pB2Uc42HaygW1TRf1h5h0lebk')
   params.append('cx', '003954572918023580770:tnjxyqfu8jq')
   params.append('q', `${q} more:pagemap:metatags-og_type:album OR more:pagemap:metatags-og_type:music.album`)
+  params.append('num', 3)
 
   const url = `https://www.googleapis.com/customsearch/v1?${params.toString()}`
 
